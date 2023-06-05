488fedda3fab96cdc67b84105a1e2e38029112e5
slyk26 <slyk26@proton.me>
2023-06-05 03:25:25 +0200

    more generic => endpoints

--- butz-api/src/routes/mod.rs
+++ butz-api/src/routes/mod.rs
@@ -1,1 +1,64 @@
 pub mod users;
+
+#[macro_export]
+macro_rules! get_all {
+    ($endpoint:literal) => {
+        #[get($endpoint)]
+        pub async fn get_all(db: &State<DB>) -> Result<Json<Vec<Model>>, Error> {
+            Ok(Json(db.get_all(TABLE)
+            .await.map_err(|_| new_error("Could not execute get_all"))?))
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! get {
+    ($endpoint:literal) => {
+        #[get($endpoint)]
+        pub async fn get(db: &State<DB>, key: &str) -> Result<Json<Model>, Error> {
+            if let Ok(Some(model)) = db.get(key).await {
+                return Ok(Json(model))
+            }
+            Err(new_error("Could not execute get"))
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! post {
+    ($endpoint:literal) => {
+        #[post($endpoint, data = "<body>")]
+        pub async fn add(db: &State<DB>, body: Model) -> Result<(Status, Json<Model>), Error> {
+            if let Ok(inserted) = db.add(TABLE, body).await {
+                return Ok((Status::Created, Json(inserted)))
+            }
+            Err(new_error("Could not execute create"))
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! delete {
+    ($endpoint:literal) => {
+        #[delete($endpoint)]
+        pub async fn delete(db: &State<DB>, key: &str) -> Result<(Status, Json<Model>), Error> {
+            if let Ok(Some(deleted)) = db.delete::<Model>(key).await {
+                return Ok((Status::Ok, Json(deleted)))
+            }
+            Err(new_error("Could not execute delete"))
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! put {
+    ($endpoint:literal) => {
+        #[put($endpoint, data = "<body>")]
+        pub async fn update(db: &State<DB>, key: &str , body: Model) -> Result<Status, Error>{
+            if let Ok(Some(_)) = db.update(key, body).await {
+                return Ok(Status::Ok)
+            }
+            Err(new_error("Could not execute update"))
+        }
+    }
+}
--- butz-api/src/routes/users.rs
+++ butz-api/src/routes/users.rs
@@ -2,46 +2,21 @@ use std::io::Error;
 use rocket::http::Status;
 use rocket::serde::json::Json;
 use rocket::State;
+
 use crate::db::DB;
+use crate::*;
 use crate::models::User;
 use crate::utils::new_error;
 
 const TABLE: &str = "users";
+type Model = User;
 
-#[get("/users")]
-pub async fn get_all(db: &State<DB>) -> Result<Json<Vec<User>>, Error> {
-    Ok(Json(db.get_all(TABLE)
-        .await.map_err(|_| new_error("Could not get users"))?))
-}
+get_all!("/users");
 
-#[get("/users/<key>")]
-pub async fn get(db: &State<DB>, key: &str) -> Result<Json<User>, Error> {
-    if let Ok(Some(user)) = db.get(key).await {
-        return Ok(Json(user))
-    }
-    Err(new_error("Could not find user"))
-}
+get!("/users/<key>");
 
-#[post("/users", data = "<user>")]
-pub async fn add(db: &State<DB>, user: User) -> Result<(Status, Json<User>), Error> {
-    if let Ok(inserted) = db.add(TABLE, user).await {
-        return Ok((Status::Created, Json(inserted)))
-    }
-    Err(new_error("Could not create user"))
-}
+post!("/users");
 
-#[delete("/users/<key>")]
-pub async fn delete(db: &State<DB>, key: &str) -> Result<(Status, Json<User>), Error> {
-    if let Ok(Some(deleted)) = db.delete::<User>(key).await {
-        return Ok((Status::Ok, Json(deleted)))
-    }
-    Err(new_error("Could not delete user"))
-}
+delete!("/users/<key>");
 
-#[put("/users/<key>", data = "<user>")]
-pub async fn update(db: &State<DB>, key: &str , user: User) -> Result<Status, Error>{
-    if let Ok(Some(_)) = db.update(key, user).await {
-       return Ok(Status::Ok)
-    }
-    Err(new_error("Could not update user"))
-}
+put!("/users/<key>");
